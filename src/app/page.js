574b09993783bc24fc142c75a08c1a6372f76bf9
Slayer007 <arafrao123@gmail.com>
2023-09-15 19:36:04 +0500

    Changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,15 @@
 import AboutMe from "@/components/About/about";
 import Experience from "@/components/Experience/experience";
 import Hero from "@/components/Hero/hero";
+import Navbar from "@/components/Navbar/navbar";
 import Projects from "@/components/Projects/projects";
-import Quote from "@/components/Quote/quote";
 import MySkills from "@/components/Skills/skills";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between lg:p-24 overflow-x-hidden ">
+    <main className="flex min-h-screen flex-col items-center justify-between overflow-x-hidden">
+      <Navbar />
       <Hero />
-      <Quote />
       <AboutMe />
       <MySkills />
       <Projects />
--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -4,72 +4,75 @@ import { useSpring, animated } from "react-spring";
 import "animate.css";
 
 export default function Hero() {
-  const fadeInTop = useSpring({
-    opacity: 1,
-    transform: "translateY(0)",
-    from: { opacity: 0, transform: "translateY(-20px)" },
-    delay: 500,
-  });
-
-  const fadeInLeft = useSpring({
-    opacity: 1,
-    transform: "translateX(0)",
-    from: { opacity: 0, transform: "translateX(-20px)" },
-    delay: 500,
-  });
-
-  const fadeInBottom = useSpring({
-    opacity: 1,
-    transform: "translateY(0)",
-    from: { opacity: 0, transform: "translateY(20px)" },
-    delay: 1500,
-  });
-
-  const fadeInRight = useSpring({
-    opacity: 1,
-    transform: "translateX(0)",
-    from: { opacity: 0, transform: "translateX(20px)" },
-    delay: 2000,
-  });
-
   return (
-    <section>
-      <div className="lg:grid lg:grid-cols-2 justify-center">
-        <div className="col-span-1 lg:my-9 my-1">
-          <animated.h1
-            className="font-bold lg:text-[32px] text-[20px] my-4 animate__animated animate__fadeInDownBig text-center lg:text-left"
-            style={fadeInTop}
-          >
-            Hi! My name is
-          </animated.h1>
+    <div
+      id="home"
+      className="relative w-full h-full bg-gray-200 overflow-hidden"
+    >
+      {/* :HERO BACKGROUND */}
+      <div className="absolute w-full h-full bg-black">
+        <img
+          src="/my_img3.png"
+          alt=""
+          className="absolute -right-28 w-full h-full object-cover"
+        />
+      </div>
 
-          <animated.div
-            className="flex flex-col justify-center items-center bg-cover bg-clip-text bg-center uppercase text-transparent lg:text-7xl text-[26px] text-center lg:text-left font-extrabold tracking-wide antialiased my-4"
-            style={{
-              backgroundImage:
-                "url(https://media.giphy.com/media/xTiTniuHdUjpOlNo1q/source.gif)",
-              ...fadeInLeft,
-            }}
-          >
-            Araf Khalid
-          </animated.div>
-          <animated.p
-            className="lg:text-left font-extrabold text-center"
-            style={fadeInBottom}
-          >
-            I am a Front End Web Developer
-          </animated.p>
+      {/* :HERO CONTAINER */}
+      <div className="relative py-10 xl:py-20 px-5 w-full sm:w-2/3 h-full flex flex-col justify-center items-center bg-white bg-opacity-90">
+        {/* ::Top Title */}
+        <h2 className="mt-14 text-center text-base sm:text-lg text-gray-800 font-newsreader font-medium uppercase tracking-extra">
+          <span className="text-3xl">A</span>spiring{" "}
+          <span className="text-3xl">F</span>ront-end{" "}
+          <span className="text-3xl">D</span>eleveloper
+        </h2>
+
+        {/* ::Title => W O R K */}
+        <div
+          className="pb-7 sm:pb-14 xl:pb-20 max-w-sm sm:max-w-xl xl:max-w-2xl w-full flex flex-col items-center overflow-hidden"
+          style={{ textShadow: "2px 2px 5px rgba(86,90,88,0.85)" }}
+        >
+          {/* :::W O */}
+          <span className="relative w-full text-7xl sm:text-8xl md:text-9xl xl:text-10xl font-semibold flex justify-around">
+            <span className="z-10">A</span>
+            <span>R</span>
+            {/* Line through */}
+            <span className="absolute top-1/2 w-5/6 h-3 md:h-6 bg-yellow-700 bg-opacity-80 shadow-lg transform -translate-x-full animate-linethrough" />
+            {/* Shadow W and O */}
+            <span className="absolute hidden md:block -bottom-8 left-40 w-16 md:w-32 h-32 bg-gray-800 bg-opacity-20 filter blur-xl" />
+            <span className="absolute hidden md:block -bottom-8 right-10 w-16 md:w-32 h-32 bg-gray-800 bg-opacity-20 filter blur-xl" />
+          </span>
+
+          {/* :::R K */}
+          <span className="relative w-full text-7xl sm:text-8xl md:text-9xl xl:text-10xl font-semibold flex justify-around">
+            <span>A</span>
+            <span className="z-10">F</span>
+            {/* Line through */}
+            <span className="absolute top-1/2 w-5/6 h-3 md:h-6 bg-yellow-700 bg-opacity-80 shadow-lg transform translate-x-full animate-linethrough" />
+            {/* Shadow R and K */}
+            <span className="absolute hidden md:block -bottom-8 left-40 w-16 md:w-32 h-32 bg-gray-800 bg-opacity-20 filter blur-xl" />
+            <span className="absolute hidden md:block -bottom-8 right-10 w-16 md:w-32 h-32 bg-gray-800 bg-opacity-20 filter blur-xl" />
+          </span>
         </div>
 
-        <animated.div
-          className="col-span-1 animate__animated animate__fadeInRight  grid  h-min-fit justify-center "
-          style={fadeInRight}
-        >
-          <div className="flex justify-center pr-10 md:pr-0 lg:w-full lg:h-full lg:grid">
-            <img src="./Saly-10.png" alt="Profile" />
-          </div>
-        </animated.div>
+        {/* ::Bottom title and text */}
+        <div className="w-3/4 flex flex-col md:flex-row">
+          {/* :::Title */}
+          <h3 className="sm:px-5 text-center md:text-right text-lg text-gray-800 font-newsreader font-normal uppercase tracking-widest">
+            <span className="text-3xl">M</span>y{" "}
+            <span className="text-3xl">V</span>ision
+          </h3>
+          {/* :::Text */}
+          <p className="sm:px-5 text-center md:text-left font-light leading-relaxed">
+            As a frontend developer, my vision is to craft visually stunning and
+            user-friendly web experiences. I aim to continually learn and adapt
+            to emerging technologies, ensuring my work enhances online
+            interactions and leaves a positive impact on users. Ultimately, I
+            aspire to contribute to innovative web solutions that make the
+            digital world more accessible and engaging
+          </p>
+        </div>
       </div>
-    </section>
+    </div>
   );
 }
